84a5f5d1566934ff72827d046b274db56cd3a818
YusifE <yusifelawawdeh@gmail.com>
2017-07-22 15:01:38 -0500

    Learning Data streaming serialzation, and parsing

---
+++ StatisticApp/ExcerciseProgram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StatisticApp
+{
+    class Excercise
+    {
+        static void AnotherConsoleExcersiseApp()
+        {
+            int runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+
+                // prompt user for minutes of exercise
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+                string entry = Console.ReadLine();
+
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+
+                    int minutes = int.Parse(entry);
+
+                    // add minutes via input for da running total\
+                    runningTotal = runningTotal + minutes;
+
+                    // display total minutes exercised
+                    Console.WriteLine("You've exercised " + runningTotal + " minutes");
+
+                    // repeat until user quits
+
+                }
+
+            }
+
+            Console.WriteLine("GoodDay sir !");
+        }
+    }
+}
