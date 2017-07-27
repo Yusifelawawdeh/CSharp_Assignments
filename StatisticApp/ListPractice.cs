8a868d171530bef6a7e2065fd5de05966d8b14a2
YusifE <yusifelawawdeh@gmail.com>
2017-07-26 21:16:00 -0500

    learning Lists

--- StatisticApp/ListPractice.cs
+++ StatisticApp/ListPractice.cs
@@ -6,8 +6,102 @@ using System.Threading.Tasks;
 
 namespace StatisticApp
 {
-    class ListPractice
+    public class ListPractice
+    {
+        public void StudentList()
+        {
+            List<string> students = new List<string>();
+            students.Add("Sue");
+            students.Add("JOE");
+            students.Add("bob");
+            students.Count();
+            students.Capacity.ToString();
+            students.Add("Mary");
+            students.Add("beth");
+        }
+
+        public void TeacherList()
+        {
+            List<string> teachers = new List<string>(500);
+            teachers.Add("jose");
+            teachers.Count();
+            teachers.Capacity.ToString();
+        }
+    }
+
+    public class DifferentLists
     {
 
+        public void CrocsList()
+        {
+            List<string> crocs = new List<string>() {"harvey", "Alexander", "Rex", "Rovers"};
+            var crocAmount = crocs.Capacity;
+            Console.Write(crocAmount);
+
+
+        }
+
+        public void AddingLists()
+        {
+            List<string> killerCrocs = new List<string>() {"GoGo", "Mojo", "Hobo", "Trolo"};
+
+            List<String> KillerCrocsNewBand = new List<string>(4) {"GoGo", "Mojo", "Hobo", "Trolo"};
+
+            List<String> KillerCrocsNewNewBand = new List<string>(KillerCrocsNewBand);
+
+        }
+
+        public void ListToArrayPractice()
+        {
+
+            List<string> killerCrocs = new List<string>() { "GoGo", "Mojo", "Hobo", "Trolo" };
+
+            string[] originalGangstaCrocs = killerCrocs.ToArray();
+
+            foreach (string ogCroc in killerCrocs)
+            {
+                Console.WriteLine(ogCroc);
+            }
+
+
+        }
+
+        public void InsertIntoAndRemoveAtList()
+        {
+
+            List<string> killerCrocs = new List<string>() { "GoGo", "Mojo", "Hobo", "Trolo" };
+
+            killerCrocs.Insert(1, "filthy frank");
+
+            Console.Write(killerCrocs);
+
+            killerCrocs.RemoveAt(2);
+
+            Console.Write(killerCrocs);
+
+        }
+
+        public void RangeAndRemoveItems()
+        {
+
+            List<string> killerCrocs = new List<string>() { "GoGo", "Mojo", "Hobo", "Trolo" };
+
+            killerCrocs.Remove("Mojo");
+
+
+        }
+
+        public void SortingLists()
+        {
+
+            List<string> killerCrocs = new List<string>() { "GoGo", "Mojo", "Hobo", "Trolo" };
+
+            killerCrocs.Remove("Mojo");
+
+
+        }
+
+
     }
+
 }
