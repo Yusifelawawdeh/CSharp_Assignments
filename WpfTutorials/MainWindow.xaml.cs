3b94595ca8fa2b1f95df007230ea80b80320e0e8
YusifE <yusifelawawdeh@gmail.com>
2017-07-30 15:36:30 -0500

    resizeability added

--- WpfTutorials/MainWindow.xaml.cs
+++ WpfTutorials/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfTutorials.ViewModel;
 
 namespace WpfTutorials
 {
@@ -23,6 +24,8 @@ namespace WpfTutorials
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new WindowViewModel(this);
         }
     }
 }
--- WpfTutorials/ViewModel/WindowViewModel.cs
+++ WpfTutorials/ViewModel/WindowViewModel.cs
@@ -14,9 +14,43 @@ namespace WpfTutorials.ViewModel
     {
         private Window _window;
 
+        private int _outerMarginSize = 10;
+
+        private int _windowRadius = 10;
+
+        public int ResizeBorder { get; set; } = 6;
+
+        public Thickness ResizeBorderThinckness => new Thickness(ResizeBorder + OuterMarginSize);
+
+        public int OuterMarginSize
+        {
+            get => _window.WindowState == WindowState.Maximized ? 0 : _outerMarginSize;
+            set => _outerMarginSize = value;
+        }
+
+        public Thickness OuterMarginSizeThickness => new Thickness(OuterMarginSize);
+
+        public int WindowRadius
+        {
+            get => _window.WindowState == WindowState.Maximized ? 0 : _windowRadius;
+            set => _windowRadius = value;
+        }
+
+        public CornerRadius WindowCornerRadius => new CornerRadius(WindowRadius);
+
         public WindowViewModel(Window window)
         {
             _window = window;
+
+            _window.StateChanged += (sender, e) =>
+            {
+                OnPropertyChanged(nameof(ResizeBorderThinckness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+
         }
 
     }
