9b69d42bfa3b037c79778964ce10ecbf8124c642
YusifE <yusifelawawdeh@gmail.com>
2017-07-30 14:44:46 -0500

    window constructor

---
+++ WpfTutorials/ViewModel/WindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfTutorials.ViewModel
+{
+    /// <summary>
+    /// view model for the custom flat window
+    /// </summary>
+    class WindowViewModel : BaseViewModel
+    {
+        private Window _window;
+
+        public WindowViewModel(Window window)
+        {
+            _window = window;
+        }
+
+    }
+}
