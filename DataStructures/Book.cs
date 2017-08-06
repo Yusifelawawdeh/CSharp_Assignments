407d41c979e3be6238b751cc67590f14438ffce0
YusifE <yusifelawawdeh@gmail.com>
2017-08-06 18:26:06 -0500

    more generics

---
+++ DataStructures/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Book
+    {
+        public string Isbn { get; internal set; }
+        public string Title { get; internal set; }
+
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+    }
+}
