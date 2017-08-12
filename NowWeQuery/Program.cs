654fffb3bddd0e53e6e78383c05af8dc3755746e
YusifE <yusifelawawdeh@gmail.com>
2017-08-12 12:14:03 -0500

    learning actions<>

--- NowWeQuery/Program.cs
+++ NowWeQuery/Program.cs
@@ -10,32 +10,40 @@ namespace NowWeQuery
 
     class Program
     {
-        delegate void SayGreeting(string name);
+        //delegate void SayGreeting(string name);
+
 
         public static void SayHello(string name)
         {
             Console.WriteLine(string.Format("Hello, {0}", name));
         }
 
-        public static void SayGoodBye(string name)
-        {
-            Console.WriteLine(string.Format("Hello, {0}", name));
+        //public static void SayGoodBye(string name)
+        //{
+        //    Console.WriteLine(string.Format("Hello, {0}", name));
 
-        }
+        //}
+
+
 
         static void Main(string[] args)
         {
-            SayGreeting sayGreeting = delegate(string name)
+            Action<string> sayGreeting;
+            sayGreeting = delegate (string name)
             {
                 Console.WriteLine(string.Format("Hello, {0}", name));
             };
-        Console.WriteLine("What's your name ?");
-        string input = Console.ReadLine();
-        sayGreeting(input);
-        Console.ReadLine();
-        sayGreeting = new SayGreeting(SayGoodBye);
-        sayGreeting(input);
-      
+            Console.WriteLine("What's your name ?");
+            string input = Console.ReadLine();
+            sayGreeting(input);
+            Console.ReadLine();
+            //sayGreeting = new SayGreeting(SayGoodBye);
+            sayGreeting = delegate (string name)
+            {
+                Console.WriteLine(string.Format("Later, {0}", name));
+            };
+            sayGreeting(input);
+
 
             ListLooperAlpha();
             ListLooper();
